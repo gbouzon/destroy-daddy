5f3e2a2c9511f5b60f7e8634ba3216764c07e331
Giuliana Bouzon <ga@bouzon.com.br>
2022-09-29 22:09:49 -0400

    added the remaining planets

--- DestroyDaddy/Assets/Space/Earth Script/MoveMoon.cs
+++ DestroyDaddy/Assets/Space/Earth Script/MoveMoon.cs
@@ -13,6 +13,6 @@ public class MoveMoon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0001f, 0.002f, 0);
+        transform.Rotate(0.0001f, 0.01f, 0);
     }
 }
