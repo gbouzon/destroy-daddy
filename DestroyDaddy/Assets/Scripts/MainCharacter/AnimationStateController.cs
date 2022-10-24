db033a4c3d7be9a9cb22bf29bf7f8410a4ef044f
danichh <70587707+danichh@users.noreply.github.com>
2022-10-24 14:18:26 -0400

    AGUOJ-19 : update the walk and run animation + add a strafe animation left and right

--- DestroyDaddy/Assets/Scripts/MainCharacter/AnimationStateController.cs
+++ DestroyDaddy/Assets/Scripts/MainCharacter/AnimationStateController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class AnimationStateController : MonoBehaviour
 {
-    Animator animator;
-    float velocity = 0.0f;
-    public float acceleration = 0.1f;
+   Animator animator;
+   float velocityZ = 0.0f;
+   float velocityX = 0.0f;
+   public float acceleration = 0.2f;
+   public float deceleration = 0.2f;
+   public float maxWalkVelocity = 0.5f;
+   public float maxRunVelocity = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,44 +21,87 @@ public class AnimationStateController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        bool fowardPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-        bool runPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)  ;
-        // bool isWalking = animator.GetBool("isWalking");
-        // bool isRunning = animator.GetBool("isRunning");
+      bool fowardPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+      bool runPressed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+      bool rightPressed = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+      bool leftPressed = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+      float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
 
-        if(fowardPressed && velocity < 1.0f){
-           // animator.SetBool("isWalking", true);
-           velocity += Time.deltaTime * acceleration;
-        }
+      changeVelocity(fowardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
+      lockOrResetVelocity(fowardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity);
+      
+      animator.SetFloat("Velocity Z", velocityZ);
+      animator.SetFloat("Velocity X", velocityX);
+      }
 
-        if(!fowardPressed && velocity > 0.0f ){
-           // animator.SetBool("isWalking", true);
-           velocity -= Time.deltaTime * acceleration;   
-        }   
+   void changeVelocity(bool fowardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity){
+        if(fowardPressed && velocityZ < currentMaxVelocity){
+         // animator.SetBool("isWalking", true);
+         velocityZ += Time.deltaTime * acceleration;
+      }
 
-        
-        if(!fowardPressed && velocity < 0.0f ){
-           // animator.SetBool("isWalking", true);
-           velocity  = 0.0f;
-        }   
+      if(leftPressed && velocityX > -currentMaxVelocity){
+         velocityX -= Time.deltaTime * acceleration;
+      }
 
-        // if(isWalking && !fowardPressed){
-        //     animator.SetBool("isWalking", false);
-        // }
+      if(rightPressed && velocityX < currentMaxVelocity){
+         velocityX += Time.deltaTime * acceleration;
+      }
 
-        // if(!isRunning && (fowardPressed && runPressed)){
-        //     animator.SetBool("isRunning", true);
-        // }
+      if(!fowardPressed && velocityZ > 0.0f ){
+         // animator.SetBool("isWalking", true);
+         velocityZ -= Time.deltaTime * deceleration;   
+      }   
 
-        // if(isRunning && (!fowardPressed || !runPressed)){
-           
-        // }
-        animator.SetFloat("Velocity", velocity);
-        Debug.Log(velocity);
-        }
+      if(!fowardPressed && velocityZ < 0.0f ){
+         // animator.SetBool("isWalking", true);
+         velocityZ  = 0.0f;
+      }   
 
-         
+      if(!leftPressed && velocityX < 0.0f){
+         velocityX += Time.deltaTime * deceleration;
+      }
 
+      if(!rightPressed && velocityX > 0.0f){
+         velocityX -= Time.deltaTime * deceleration;
+      } 
+   }
 
-    
+   void lockOrResetVelocity(bool fowardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity){
+      if(!leftPressed && !rightPressed && 
+      velocityX != 0.0f  &&
+      (velocityX > -0.05f && velocityX < 0.05f)){
+         velocityX  = 0.0f;
+      }
+
+      if(fowardPressed && runPressed && velocityZ > currentMaxVelocity){
+         velocityZ = currentMaxVelocity;
+      }else if(fowardPressed && velocityZ > currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.5f)){
+          velocityZ -= Time.deltaTime * deceleration;
+      }
+
+      if(leftPressed && runPressed && velocityX < -currentMaxVelocity){
+         velocityX = -currentMaxVelocity;
+      }else if(leftPressed && velocityX < -currentMaxVelocity){
+         velocityX += Time.deltaTime * deceleration;
+         if(velocityX < -currentMaxVelocity && velocityX > (-currentMaxVelocity - 0.05f)){
+            velocityX = -currentMaxVelocity;
+         }
+      }else if(leftPressed && velocityX > -currentMaxVelocity && velocityX < (-currentMaxVelocity + 0.05f)){
+         velocityX = -currentMaxVelocity;
+      }
+
+      if(rightPressed && runPressed && velocityX > currentMaxVelocity){
+         velocityX = currentMaxVelocity;
+      }else if(rightPressed  && velocityX > currentMaxVelocity){
+         velocityX += Time.deltaTime * deceleration;
+         if(velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity - 0.05f)){
+            velocityX = -currentMaxVelocity;
+         }
+      }else if(rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity + 0.05f)){
+         velocityX = currentMaxVelocity;
+      }
+   }   
+
+      
 }
