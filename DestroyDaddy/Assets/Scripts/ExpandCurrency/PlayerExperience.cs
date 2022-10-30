f790b02437e00e00a86f19df8c8f67c5d60f4da6
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-10-29 23:21:54 -0400

    AGOUJ-37 experience increase GUI and Script

--- DestroyDaddy/Assets/Scripts/ExpandCurrency/PlayerExperience.cs
+++ DestroyDaddy/Assets/Scripts/ExpandCurrency/PlayerExperience.cs
@@ -1,18 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerExperience : MonoBehaviour
 {
+    private float health;
+    private float lerpTimer;
+    public float maxHealth = 100;
+    public float chipSpped = 2f; // controll the delaided bar take to catch up to the other
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    public TextMeshProUGUI healthText;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(Random.Range(5,10));
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            RestoreHealth(Random.Range(5,10));
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if (fillBack > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpped;
+            percentComplete = percentComplete * percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, hFraction, percentComplete);
+        }
+
+        if (fillFront < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpped;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, backHealthBar.fillAmount, percentComplete);
+        }
+
+        healthText.text = Mathf.Round(health) + "/" + Mathf.Round(maxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+
+    public void IncreaseHealth(int level)
+    {
+        maxHealth += (health * 0.01f) * ((100 - level) * 0.1f);
+        health = maxHealth;
     }
 }
