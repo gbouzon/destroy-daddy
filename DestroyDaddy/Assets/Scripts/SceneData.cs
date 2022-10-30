c3808b3ea4766cec89ddba87eee1821e5f98b790
Tony Nadeau <thegoldenrailway@gmail.com>
2022-10-30 11:19:33 -0400

    AGUOJ-39: reversing SceneData approach

--- DestroyDaddy/Assets/Scripts/SceneData.cs
+++ DestroyDaddy/Assets/Scripts/SceneData.cs
@@ -6,14 +6,4 @@ public static class SceneData
 {
     public static string previousScene = "Null";
     public static string currentScene = "Null";
-    public static Dictionary<string, Transform> spacialPostions = new Dictionary<string, Transform>();
-    // spacialPostions["Earth"] = GameObject.Find("Earth").transform;
-    // spacialPostions.Add("Moon", GameObject.Find("Moon").transform);
-    // spacialPostions.Add("Sun", GameObject.Find("Sun").transform);
-    // spacialPostions.Add("Planet1", GameObject.Find("Planet1").transform);
-    // spacialPostions.Add("Planet2", GameObject.Find("Planet2").transform);
-    // spacialPostions.Add("Planet3", GameObject.Find("Planet3").transform);
-    // spacialPostions.Add("Planet4", GameObject.Find("Planet4").transform);
-    // spacialPostions.Add("Planet5", GameObject.Find("Planet5").transform);
-
 }
