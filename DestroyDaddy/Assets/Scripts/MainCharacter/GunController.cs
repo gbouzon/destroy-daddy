f3644bbb09bd40e213e1d45d4afc99dcfb51ef6f
danichh <70587707+danichh@users.noreply.github.com>
2022-11-11 14:35:40 -0500

    AGUOJ-33: Fix aim rotation bug

--- DestroyDaddy/Assets/Scripts/MainCharacter/GunController.cs
+++ DestroyDaddy/Assets/Scripts/MainCharacter/GunController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
+using UnityEngine.Animations.Rigging;
 public class GunController : MonoBehaviour
 {   
     [SerializeField]
@@ -18,11 +19,14 @@ public class GunController : MonoBehaviour
 
     [SerializeField]
     private GameObject muzzle;
+    public float turnSpeed = 20;
     RaycastHit hit;
 
     [SerializeField]
     private  CinemachineVirtualCamera  aimCamera; 
     private float timer;
+    float rotationVelocity;
+    [SerializeField] Rig aimLayer;
 
     
    
@@ -49,14 +53,18 @@ public class GunController : MonoBehaviour
          if(mainCharacterScript.getIsAim()){
             aimCamera.gameObject.SetActive(true);
             mainCharacterScript.setRotateOnMove(false);
+           /// aimLayer.weight += Time.deltaTime / 0.3f;
+            aimRotation();
+ 
+            
 
-            Vector3 worldAimTarget = targetPosition;
-            worldAimTarget.y = transform.position.y;
-            Vector3 aimDir = (worldAimTarget - transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, aimDir, Time.deltaTime * 20f);
         }else{
             aimCamera.gameObject.SetActive(false);
             mainCharacterScript.setRotateOnMove(true);
+        }
+
+        if(Input.GetMouseButton(0)){
+            aimRotation();
         }
     }
 
@@ -65,12 +73,16 @@ public class GunController : MonoBehaviour
          if(timer >= fireRate){
             if (Input.GetMouseButton(0))
             {
-                // to-do
-                /**
-                if(tranform.postion.y != mouseWorlfPostion){
-                    turn to the TargetPostion
-                }
-                */
+                //mainCharacterScript.setRotateOnMove(false);    
+                
+            
+                Debug.Log("Player foward Position: " + transform.forward);
+            //     Debug.Log("Crosshair Position: " + targetPosition);
+            //     //if(transform.forward != targetPosition){
+            //        Debug.Log("Player Should Turn");
+            //        aimRotation();
+            //    // }
+               
 
                 timer = 0f;
                 if(hit.collider.gameObject.tag == "Enemy"){
@@ -84,11 +96,21 @@ public class GunController : MonoBehaviour
                 }
 
                 Vector3 aimDir = (targetPosition - firePoint.position).normalized;
-                GameObject laser = GameObject.Instantiate(muzzle, firePoint.position, Quaternion.LookRotation(aimDir, Vector3.up)) as GameObject;
+                Quaternion rotation = Quaternion.LookRotation(aimDir,  Vector3.up);
+                GameObject laser = GameObject.Instantiate(muzzle, firePoint.position, rotation) as GameObject;
                 laser.GetComponent<ShotBehavior>().setTarget(hit.point);
-                GameObject.Destroy(laser, 1f);
+                GameObject.Destroy(laser, 0.5f);
             }
         }
+    }
+
+    void aimRotation(){ 
+        Vector3 worldAimTarget = targetPosition;
+        worldAimTarget.y = transform.position.y;
+        Vector3 aimDir = worldAimTarget - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(aimDir,  Vector3.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
     }
     
 }
