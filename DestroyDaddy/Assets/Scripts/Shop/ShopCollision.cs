85aee804b70ac9735d02a55107cd250cc6f20237
Chilka Castro <chilcjcastro@yahoo.ca>
2022-11-20 23:53:21 -0500

    AGUOJ-60: Started working on fuel rechage on home worlds

--- DestroyDaddy/Assets/Scripts/Shop/ShopCollision.cs
+++ DestroyDaddy/Assets/Scripts/Shop/ShopCollision.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ShopCollision : MonoBehaviour
@@ -8,34 +9,63 @@ public class ShopCollision : MonoBehaviour
     [SerializeField]
     GameObject enterShopCanvas;
     [SerializeField]
-    GameObject shopCanvas;
+    GameObject fuelRechargeCanvas;
+    [SerializeField]
+    GameObject popUpWindow;
+    [SerializeField]
+    GameObject shop;
     [SerializeField]
     GameObject gunCrossHair;
+
+    void Update() {
+        Debug.Log("Max Fuel: " + ShipController.maxFuel);
+        Debug.Log("Current Fuel: " + ShipController.fuel);
+
+    }
+
     void OnTriggerEnter(Collider col) {
-        if(col.gameObject.name == "Shop" || col.gameObject.name == "FuelRecharge"){
+        if(col.gameObject.name == "ShopTrigger"){
             enterShopCanvas.SetActive(true);
+        }
+        if (col.gameObject.name == "FuelTrigger" || col.gameObject.name == "FuelRecharge") {
+            fuelRechargeCanvas.SetActive(true);
         }
     }
 
     void OnTriggerStay(Collider col) {
-        if(col.gameObject.name == "Shop" || col.gameObject.name == "FuelRecharge"){
+        if(col.gameObject.name == "ShopTrigger"){
             if (Input.GetKey(KeyCode.F)) {
-                shopCanvas.SetActive(true);
+                enterShopCanvas.SetActive(false);
+                gunCrossHair.SetActive(false);
+                shop.SetActive(true);
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
+            }
+        }
+        if (col.gameObject.name == "FuelTrigger" || col.gameObject.name == "FuelRecharge") {
+            if (Input.GetKey(KeyCode.F)) {
+                fuelRechargeCanvas.SetActive(false);
                 gunCrossHair.SetActive(false);
+                ShipController.fuel = ShipController.maxFuel;
+                Debug.Log("Current Fuel: " + ShipController.fuel);
+                popUpWindow.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
             }
-            if (Input.GetKey(KeyCode.Escape)) {
-                shopCanvas.SetActive(false);
-                Cursor.visible = false;
-                gunCrossHair.SetActive(true);
-            }
+        }
+        if (Input.GetKey(KeyCode.Escape)) {
+            popUpWindow.SetActive(false);
+            Cursor.visible = false;
+            gunCrossHair.SetActive(true);
         }
     }
 
     void OnTriggerExit(Collider col) {
-        if(col.gameObject.name == "Shop" || col.gameObject.name == "FuelRecharge"){
+        if (shop != null) {
             enterShopCanvas.SetActive(false);
+            shop.SetActive(false);
         }
+        popUpWindow.SetActive(false);
+        fuelRechargeCanvas.SetActive(false);
     }
 }
